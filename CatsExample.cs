1264cbd99c65599b9855e8592d422304186b1811
Andreas Remar <andreas.remar@gmail.com>
2017-10-01 22:13:59 +0200

    Initial commit

---
+++ CatsExample.cs
@@ -0,0 +1,38 @@
+using System;
+using SDL2;
+using cats;
+
+namespace catscsexamples {
+	public class CatsExample {
+		Cats cats;
+
+		public static int Main() {
+			CatsExample example = new CatsExample ();
+			example.Setup ();
+
+			SDL.SDL_Event Event;
+			bool running = true;
+			while (running) {
+				while (SDL.SDL_PollEvent(out Event) != 0) {
+					if (Event.type == SDL.SDL_EventType.SDL_QUIT) {
+						running = false;
+					}
+				}
+			}
+
+			example.Quit ();
+
+			return 0;
+		}
+
+		public void Setup() {
+			SDL.SDL_Init (SDL.SDL_INIT_VIDEO);
+			cats = new Cats ();
+			cats.Init (640, 480);
+		}
+
+		public void Quit() {
+			SDL.SDL_Quit ();
+		}
+	}
+}
